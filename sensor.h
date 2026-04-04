fde88744cb4ed5422cfa847dce664b111f6b4885
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-05 02:50:59 +0700

    Delete sensor.h

--- sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSOR_H
-#define SENSOR_H
-#include "common.h"
-#include <time.h>
-int readConfig(const char *filename, Sensor sensors[], int *numSensors);
-float receiveData(int sensorID);
-int TimetoSendData(Sensor *s);
-int isSensorDisconnected(Sensor s);
-#endif
