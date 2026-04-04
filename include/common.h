214763ad1e24f4e66e21d62e71b6b193487d4fb3
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-05 02:46:22 +0700

    Update common.h

--- include/common.h
+++ include/common.h
@@ -1,1 +1,27 @@
-
+#ifndef COMMON_H
+#define COMMON_H
+#define MAX_SENSORS 30
+#define BUFFER_SIZE 100
+#include <time.h>
+typedef struct{
+	int id;
+	char type[20];
+	float threshold;
+	int period;
+	time_t lastTime;
+	char location[30];
+}Sensor;
+typedef struct{
+	float data[BUFFER_SIZE];
+	int head;
+	int tail;
+	int count;
+}Buffer;
+typedef struct{
+	int validCount;
+	int errorCount;
+	int exceedCount;
+	int bufferOverflowCount;
+	int disconnectCount;
+}Stats;
+#endif
