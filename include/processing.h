6c5cb9bc74cb533aefa5d8e41f7b0dfd3fdb40f5
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-05 15:44:09 +0700

    Update processing.h

--- include/processing.h
+++ include/processing.h
@@ -1,6 +1,8 @@
 #ifndef PROCESSING_H
 #define PROCESSING_H
 #include "common.h"
+#include "sensor.h"
+#include "buffer.h"
 int isValidData(float value);
 int checkThreshold(Sensor s, float value);
 float filterData(Buffer *b);
