a7c11a709a5db6d2c2fd739aaaec58d98ef65f65
tessia252 <blowthetempest@gmail.com>
2026-04-06 21:02:53 +0700

    Add files via upload

--- include/sensor.h
+++ include/sensor.h
@@ -3,7 +3,7 @@
 #include "common.h"
 #include <time.h>
 int readConfig(const char *filename, Sensor sensors[], int *numSensors);
-float receiveData(int sensorID);
+float receiveData(int sensorID, const char* type);
 int TimetoSendData(Sensor *s);
 int isSensorDisconnected(Sensor s);
 #endif
