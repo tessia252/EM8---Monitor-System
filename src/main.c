e815704b181b6ca4f81bf5825883ce3e50436072
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-07 00:13:10 +0700

    Update main.c
    them logmessage de log cac dong trong terminal

--- src/main.c
+++ src/main.c
@@ -3,116 +3,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <string.h>
 #include "sensor.h"
 #include "buffer.h"
 #include "processing.h"
 #include "stats.h"
 
 int main() {
-
-    // kh?i t?o random
     srand(time(NULL));
-
-    // khai báo
     Sensor sensors[MAX_SENSORS];
     int numSensors = 0;
-
+    char msg[200];
     Buffer buffer; 
     Stats stats;
-
-    // kh?i t?o
     initBuffer(&buffer);
     initStats(&stats);
 
-    // ð?c config
     if (readConfig("config.txt", sensors, &numSensors) != 0) {
-        printf("Khong doc duoc file config\n");
+        printf("Loi: Khong tim thay file config.txt\n");
         return 1;
     }
+    FILE *logFile = fopen("activity_log.txt", "a");
+if (logFile == NULL) {
+    printf("Khong mo duoc file log\n");
+    return 1;
+}
 
-    printf("Da doc %d sensors\n", numSensors);
-
-    // v?ng l?p chính
+    printf("He thong san sang voi %d cam bien\n", numSensors);
     while (1) {
-
-        // 1. sensor g?i d? li?u
-        for (int i = 0; i < numSensors; i++) {
-
+		int i;
+		time_t now = time(NULL);
+        struct tm *t = localtime(&now);
+        for (i = 0; i < numSensors; i++) {
             if (isSensorDisconnected(sensors[i])) {
-
-                printf("Sensor %d bi mat ket noi\n",
-                       sensors[i].id);
-
+                sprintf(msg,"[%02d:%02d:%02d] Sensor ID %d: Mat ket noi", t->tm_hour, t->tm_min, t->tm_sec, sensors[i].id);
+                printf("%s\n",msg);
+                logMessage(logFile,msg);
                 recordDisconnect(&stats);
-
                 continue;
             }
 
             if (TimetoSendData(&sensors[i])) {
-
-                float value =
-                    receiveData(sensors[i].id);
-
-                // ki?m tra buffer ð?y
+                float value = receiveData(sensors[i].id,sensors[i].type);
+                
                 if (isBufferFull(&buffer)) {
-
                     recordOverflow(&stats);
-
                 }
-
-                pushBuffer(&buffer, value);
-
-                printf("Nhan data tu sensor %d: %.2f\n",
-                       sensors[i].id,
-                       value);
-            }
-        }
-
-        // 2. x? l? d? li?u t? buffer
-        float data;
-
-        if (popBuffer(&buffer, &data)) {
-
-            if (!isValidData(data)) {
-
-                printf("Data loi: %.2f\n", data);
-
-                recordError(&stats);
-
-            }
-            else {
-
-                recordValid(&stats);
-
-                // ki?m tra vý?t ngý?ng
-                if (checkThreshold(sensors[0], data)) {
-
-                    printf("Vuot nguong!\n");
-
-                    recordExceed(&stats);
-                }
-
-                float avg =
-                    filterData(&buffer);
-
-                printf("Gia tri trung binh: %.2f\n",
-                       avg);
-            }
-        }
-
-        // in report m?i 20 v?ng
-        static int loop = 0;
-
+                pushBuffer(&buffer, value);        
+	            float data;
+		        if (popBuffer(&buffer, &data)) {
+		        	sprintf(msg,"[%02d:%02d:%02d] Sensor ID %d (%s): %.2f ", t->tm_hour, t->tm_min, t->tm_sec, sensors[i].id, sensors[i].type, data);
+		        	printf("%s\n",msg);
+                	logMessage(logFile,msg);
+		            if (!isValidData(data)) {
+		                sprintf(msg,"Du lieu loi!");
+		                printf("%s\n",msg);
+		                recordError(&stats);
+		            }
+		            else {
+		                recordValid(&stats);
+		                if (checkThreshold(sensors[i], data)) {
+		                    sprintf(msg,"[CANH BAO: Vuot nguong!]");
+		                    printf("%s\n",msg);
+                			logMessage(logFile,msg);
+		                    recordExceed(&stats);
+		                }
+		                else {
+                            sprintf(msg,"-> [OK]");
+                            printf("%s\n",msg);
+               				logMessage(logFile,msg);
+                        }
+		                float avg = filterData(&buffer);
+		                if (avg != 0){
+		                	sprintf(msg,"Trung binh truot: %.2f", avg);
+		                	printf("%s\n",msg);
+                			logMessage(logFile,msg);
+						}
+		       		}
+		    	}
+        	}
+    	}
+		static int loop = 0;
         loop++;
-
         if (loop % 20 == 0) {
-
-            printReport(stats);
-
+            printReport(stats,logFile);
         }
-		Sleep(3000);
-    }
-
+		Sleep(1000);
+	}
+	fclose(logFile);
     return 0;
 }
