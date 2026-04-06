7105c394896721dfce833c92142cbfd941633f3d
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-07 00:15:08 +0700

    Update common.h

--- include/common.h
+++ include/common.h
@@ -2,6 +2,7 @@
 #define COMMON_H
 #define MAX_SENSORS 30
 #define BUFFER_SIZE 100
+#include<stdio.h>
 #include <time.h>
 typedef struct{
 	int id;
@@ -19,9 +20,14 @@ typedef struct{
 }Buffer;
 typedef struct{
 	int validCount;
-	int errorCount;
-	int exceedCount;
-	int bufferOverflowCount;
-	int disconnectCount;
-}Stats;
+    int errorCount;
+    int disconnectCount;
+    int exceedCount;
+    int bufferOverflowCount;
+    int errorBySensor[MAX_SENSORS];
+    float maxTemp;
+    float maxHumidity;
+    FILE *logFile;
+} Stats;
+
 #endif
