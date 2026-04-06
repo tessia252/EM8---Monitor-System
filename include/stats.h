b89eb492dda0b41e01613cea87099f0fab361d60
Hanh1411 <ANH.NPH2513963@sis.hust.edu.vn>
2026-04-07 00:15:43 +0700

    Update stats.h
    khai bao them logmessage

--- include/stats.h
+++ include/stats.h
@@ -1,12 +1,41 @@
 #ifndef STATS_H
 #define STATS_H
+
 #include "common.h"
+#include "sensor.h"
+#include <stdio.h>
+
+void logMessage(FILE *logFile,const char *msg);
 void initStats(Stats *s);
-void updateStats(Stats *s, float value);
+
 void recordValid(Stats *s);
+
+/* sua: thêm sensorID */
+
+void recordError(
+    Stats *s
+);
+
+void recordDisconnect(
+    Stats *s
+);
+
 void recordExceed(Stats *s);
-void recordDisconnect(Stats *s);
-void recordError(Stats *s);
+
 void recordOverflow(Stats *s);
-void printReport(Stats s);
+
+/* s?a: thêm Stats* */
+
+void updateMaxValue(
+    Stats *s,
+    Sensor sensor,
+    float value
+);
+
+void printReport(Stats s, FILE *logFile);
+
+/* thêm hàm ðóng file */
+
+void closeStats(Stats *s);
+
 #endif
